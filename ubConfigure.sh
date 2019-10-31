0995df917aec995bd6f5a7744adc42395113dd3b
Patrick FARRUGIA <pfarrugia@determine.com>
2019-10-31 15:50:29 +0100

    Add libreoffice, docker, vagrant installations

--- ubConfigure.sh
+++ ubConfigure.sh
@@ -1,6 +1,3 @@
-# install the import binaries
-sudo apt-get install -y git git-flow
-
 # add repo for atom
 sudo apt-get -y install wget
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
@@ -8,18 +5,41 @@ sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ 
 sudo apt-get update
 
 # install basic env and dev tools
-sudo apt-get --assume-yes install htop zsh rxvt-unicode terminator
-sudo apt-get --assume-yes install git git-flow subversion
-sudo apt-get --assume-yes install curl
-sudo apt-get --assume-yes install vim nano atom
+sudo apt-get -y install htop zsh rxvt-unicode terminator \
+git git-flow subversion \
+install curl \
+vim nano atom
 
 # install of phpstorm
-sudo apt-get --assume-yes install snapd snapd-xdg-open
+sudo apt-get -y install snapd snapd-xdg-open
 sudo snap install phpstorm --classic
 
 # libreoffice
-# docker
+sudo snap install libreoffice
+
+# docker (I approximatively copied and pasted the official instructions of docker)
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+#use " disco " if " eoan " (19.10) doesn't work 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+#installation of docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+
+
 # vagrant
+#virtualbox is mandatory
+sudo apt install virtualbox
+sudo apt install vagrant
+
 #i3
 
 #___________________________________________________

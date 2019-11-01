98c49087c7fccf35fe72c79d0d2e5c775472fcf5
Patrick FARRUGIA <pfarrugia@determine.com>
2019-11-01 13:49:17 +0100

    Add vhosts

---
+++ add-vhosts.sh
@@ -0,0 +1,4 @@
+sudo echo '192.168.10.10   local.devdetermine.com' >> /etc/hosts
+sudo echo '192.168.10.10   local72.devdetermine.com' >> /etc/hosts
+sudo echo '192.168.10.10   local73.devdetermine.com' >> /etc/hosts
+sudo echo '192.168.10.10   devdetermine.com.local' >> /etc/hosts
